931f4df99317d88f5a508c7555ad9596986ee6fa
dohdong <96772776+dohdong@users.noreply.github.com>
2022-01-17 14:37:25 +0900

    Add files via upload

---
+++ 01_17/for/while1.java
@@ -0,0 +1,14 @@
+
+public class while1  {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int total = 0;
+
+		while(i < 10) {
+			i = i + 1;
+			total =  total + i ;
+		}
+		System.out.println(total) ;
+	}
+}
