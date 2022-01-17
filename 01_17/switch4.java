a47302b06d63a4470be3d0279f50d60ee796b9dc
dohdong <96772776+dohdong@users.noreply.github.com>
2022-01-17 13:08:01 +0900

    Add files via upload

---
+++ 01_17/switch4.java
@@ -0,0 +1,20 @@
+//import java.util.Scanner;
+//
+//
+//public class switch4 {
+//	
+//	public static void main(String[] args) {
+//		Scanner input = new Scanner(System.in) ;
+//		System.out.print("년도를 입력하세요 : " );	
+//		int num = input.nextInt();	
+//		
+//		switch (num)
+//		
+//		
+//		
+//		
+//	}
+//}
+
+
+// 조건이 == 만 되기때문에 윤년은 안된다는 사실~
