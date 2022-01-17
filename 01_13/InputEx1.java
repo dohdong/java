1ad0088ad3c1f06a8209614a6c3e82027fd90190
dohdong <96772776+dohdong@users.noreply.github.com>
2022-01-17 09:52:08 +0900

    Add files via upload

--- 01_13/InputEx1.java
+++ 01_13/InputEx1.java
@@ -6,14 +6,21 @@ public class InputEx1 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in) ;
-		System.out.println("1~100사이의 정수 입력 : " );
-		int num = input.nextInt();
+		System.out.print("1~100사이의 정수 입력 : " ); // 입력된값은 정수열이므로
+		int num = input.nextInt();				   // int로 변환해줌
 		// 짝수/홀수 check 출력
 		if (num%2 == 0 ){
-			System.out.println("짝수입니다.");
+			System.out.printf("%d은 짝수입니다.\n", num); 
+			
+			// form의 형태를 정해주는 출력방식..
+			// 그래서 printf만 가능하고, 줄바꿈은 뒤에 \n으로 해줌.
+			
+			// System.out.printf("%1$3d은 짝수입니다.\n", num); 
+			// 여기서 1$는 첫번째 argument, 3d는 3자리까지 표현. 
+			
 		}
 		else {
-			System.out.println("홀수입니다.");
+			System.out.println(num+"는 홀수입니다.");
 			
 		}
 		input.close(); 
--- 01_13/InputEx2.java
+++ 01_13/InputEx2.java
@@ -13,9 +13,13 @@ public class InputEx2 {
 		int num2 = input.nextInt();
 		System.out.print("연산자 입력(+,-,*,/,%) : " );	
 		String operator = input.next();
+		// nextLine() 도 있는데 엔터 전까지 다 받아들인다..? 잘 모름.
+		// op.charAt(0), op.trim() -> 메서드 들임.
 		
+		// char operator = op.charAt(0);   , op.trim()    // charAt은 잘 모르겠음 ㅋㅋ
+	    // if (op.charAt(0)=='+'){     //op.trim().equals("+") // 그냥 아래 방식대로 하자.
 		if (operator.equals("+")) {
-			System.out.println("결과는"+num+"+"+num2+"="+(num + num2)+"입니다.");
+			System.out.printf("%1$3d + %2$3d =%3$3d\n",  num, num2, (num+num2));
 		} else if (operator.equals("-")) {
 			System.out.println("결과는"+num+"-"+num2+"="+(num - num2)+"입니다.");
 		} else if (operator.equals("*")) {
